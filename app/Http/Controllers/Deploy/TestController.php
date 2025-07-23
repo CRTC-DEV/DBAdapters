766b7c0864b824358dbebe0bcccce34d0e258de5
Chris <baotrungas99@gmail.com>
2025-07-23 11:09:16 +0700

    test ci/cd

--- app/Http/Controllers/Deploy/TestController.php
+++ app/Http/Controllers/Deploy/TestController.php
@@ -10,6 +10,6 @@ class TestController extends Controller
 {
     public function index()
     {
-        dd('This is a test route! testt');
+        dd('This is a test route! testtaadasdasdasd');
     }
 }
